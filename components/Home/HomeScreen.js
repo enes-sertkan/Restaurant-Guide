9e1f09149378bd6f71bb1459be0e5ec42b75434e
Humaiyun Uddin <uddin513@msn.com>
2022-12-08 17:46:28 -0500

    Implemented initial db creation and retrieval in useeffect,

--- components/Home/HomeScreen.js
+++ components/Home/HomeScreen.js
@@ -1,9 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Text, SafeAreaView, ActivityIndicator } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-const HomeScreen = () => {
+import * as SQLite from "expo-sqlite";
+
+const HomeScreen = ({ navigation }) => {
   const [isLoading, setIsLoading] = useState(false);
+
+  const db = SQLite.openDatabase("restaurantdb.db");
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(`CREATE TABLE IF NOT EXISTS restaurant (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT(128), address TEXT(512), phone TEXT(32), rating TEXT(32), description TEXT(1024))`);
+    });
+
+    const listen = navigation.addListener("focus", () => {
+
+      // TODO: replace null with pre-made restaurants data
+      db.transaction(tx => {
+        tx.executeSql(`SELECT * FROM restaurant`, null, 
+          (txObj, res) => setResult(res.rows._array),
+          (txObj, err) => console.log(err)
+        );
+      });
+
+      console.log("Home Screen")
+    })
+
+    return listen;
+  }, [navigation])
+
+  const showRestaurants = () => {
+    return result.map((restaurant, index) => {
+      return (
+        <>
+          <View key={index} style={styles.row}>
+            <Text>Name: {restaurant.name} </Text>
+            <Text>Address: {restaurant.address}</Text>
+            <Text>Phone: {restaurant.phone}</Text>
+            <Text>Rating: {restaurant.rating}</Text>
+            <Text>Description: {restaurant.description}</Text>
+          </View>
+        </>
+      )
+    })
+  }
 
   if(isLoading) {
     return (
@@ -15,11 +57,9 @@ const HomeScreen = () => {
 
     return (
       <SafeAreaView style={styles.container}>
-            <Text styles={styles.heading}>Your Restaurants</Text>
-
         <View>
             <Text>Home Page</Text>
-            <StatusBar style="auto" />
+            {showRestaurants()}
         </View>
       </SafeAreaView>
     )
@@ -36,6 +76,15 @@ const styles = StyleSheet.create({
       fontSize: 20,
       marginTop: 3,
     },
+    row: {
+      borderWidth: 1,
+      padding: 5,
+      flexDirection: "column",
+      alignItems: "center",
+      alignSelf: "stretch",
+      justifyContent: "space-between",
+      margin: 8
+    }
   });
 
 export default HomeScreen
