e874c1661706268ed171c80046e12ed733c9fdca
Humaiyun Uddin <uddin513@msn.com>
2022-12-08 12:18:05 -0500

    Implemented the necessary textinputs and styles of the form

--- components/Add/AddScreen.js
+++ components/Add/AddScreen.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native';
+import AddImage from "../../assets/add.png";
 
 const AddScreen = () => {
     return (
-        <View style={styles.container}>
-            <Text>Add Page</Text>
-            <StatusBar style="auto" />
-        </View>
+      <SafeAreaView style={styles.container}>
+          <Image source={AddImage}
+            style={{ 
+              width: "100%", 
+              height: 150
+            }}
+            resizeMode="contain"
+          />
+          <Text style={styles.heading}>Add a new restaurant</Text>
+        <ScrollView style={styles.scrollView}>
+          <TextInput style={styles.input} placeholder='Restaurant Name' />
+          <TextInput style={styles.input} placeholder='Address' />
+          <TextInput style={styles.input} keyboardType="phone-pad" placeholder='Phone Number' />
+          <TextInput style={styles.input} keyboardType="numeric" placeholder='Rating' />
+          <TextInput style={[styles.input, styles.descriptionInput]} placeholder='Description' multiline numberOfLines={10}/>
+        </ScrollView>
+        
+      </SafeAreaView>
+        
     )
 }
 
@@ -16,8 +31,28 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      // justifyContent: 'center',
     },
+    scrollView: {
+      width: "100%",
+    },
+    heading: {
+      fontSize: 24,
+      marginTop: 3,
+    },
+    input: {
+      borderWidth: 1,
+      alignSelf: 'stretch',
+      margin: 10,
+      height: 50,
+      borderRadius: 10,
+      padding: 15
+
+    },
+    descriptionInput: {
+      height: 120,
+      textAlignVertical: "top"
+    }
   });
 
 export default AddScreen
