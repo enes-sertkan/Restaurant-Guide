a99f0d54e8e30a67fb810a764727660386c7e7c8
Shakil <shakil_miah@hotmail.com>
2022-12-08 18:07:20 -0500

    finished about screen

--- components/About/AboutScreen.js
+++ components/About/AboutScreen.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import AboutCard from './AboutCard';
+import shakilImage from './images/shakil.jpg';
+import humaiyunImage from './images/humaiyun.jpg';
 
 const AboutScreen = () => {
     return (
+      <ScrollView>
         <View style={styles.container}>
-        <AboutCard />
-            <Text>About Page</Text>
+          <AboutCard
+            name="Shakil Miah (101308549)"
+            jobTitle="Senior Developer"
+            image={shakilImage} 
+
+          />
+          <AboutCard
+            name="Humaiyun Uddin (101309829)"
+            jobTitle="Senior Developer"
+            image={humaiyunImage} 
+
+          />
+            
             <StatusBar style="auto" />
         </View>
+        </ScrollView>
     )
 }
 
