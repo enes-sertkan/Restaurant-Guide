bb776bb95ebcd4bd3314927e8d8d6f3ba5cdcd4b
Humaiyun Uddin <uddin513@msn.com>
2022-12-09 18:56:28 -0500

    search button now updates based on query

--- components/Search/SearchScreen.js
+++ components/Search/SearchScreen.js
@@ -1,28 +1,86 @@
-import React, { useState } from 'react';
-import { View, TextInput, Text, Button, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { View, TextInput, Text, Button, StyleSheet, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
+import Ionic from "react-native-vector-icons/Ionicons";
+import * as SQLite from "expo-sqlite";
+
 
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  // const [isPressed, setIsPressed] =  useState(false);
 
+  const [db, setDb] = useState(SQLite.openDatabase("restaurantdb.db"));
+  const [result, setResult] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
 
   const handleSearch = () => {
     // Perform the search and update the results state
-    
+    //`SELECT * FROM restaurant WHERE name LIKE '% ? %' OR name LIKE '? %' OR name LIKE '% ?' OR name LIKE '?'`, [searchTerm]
+    setSearchResult(result.filter(res => {
+      const name = res.name.toLowerCase();
+      const description = res.description.toLowerCase();
+      const searched = searchTerm.toLowerCase();
+
+      return name.includes(searched) || description.includes(searched);
+    }));
+    console.log(searchResult)
   };
 
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(`SELECT * FROM restaurant ORDER BY id DESC`, null,
+        (txObj, res) => setResult(res.rows._array),
+        (txObj, err) => console.log(err)
+      );
+    });
+  }, []);
+
+  const handleChangedText = (text) => {
+    setSearchTerm(text);
+    setSearchResult([]);
+  }
+
     return (
-      <View style={styles.container}>
-        <TextInput
-          value={searchTerm}
-          onChangeText={setSearchTerm}
-          style={styles.searchInput}
-          placeholder="Search"
-        />
-        <Button onPress={handleSearch} title="Search" />
-        <Text>{searchTerm}</Text>
-      </View>
+      <SafeAreaView>
+        <ScrollView>
+          <View style={styles.container}>
+              <TextInput
+                value={searchTerm}
+                onChangeText={(text) => handleChangedText(text)}
+                style={styles.searchInput}
+                placeholder="Search"
+              />
+              <TouchableOpacity onPress={ handleSearch }>
+                <Ionic name="search" size={30} />
+              </TouchableOpacity>
+
+              {/* <Button onPress={() => console.log(searchResult)} title="CONSOLE LOG" /> */}
+
+              {/* <Text>{searchTerm}</Text> */}
+
+            {
+              searchResult.map((restaurant, index) => (
+                <Text key={index}>{restaurant.name}</Text>
+              ))
+            }
+
+          </View>
+        
+
+        </ScrollView>
+      </SafeAreaView>
     )
+}
+
+const SearchCard = ({ name, jobTitle, image }) => {
+  return (
+    <View style={styles.cardContainer}>
+    <Image style={styles.picture} source={image} />
+    <View>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.jobTitle}>{jobTitle}</Text>
+    </View>
+  </View>
+  )
 }
 
 const styles = StyleSheet.create({
