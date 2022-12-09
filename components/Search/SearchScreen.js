44733e55e4c2236a879c7a331987ce9df39db793
Shakil <shakil_miah@hotmail.com>
2022-12-08 21:01:44 -0500

    added search template

--- components/Search/SearchScreen.js
+++ components/Search/SearchScreen.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Text, FlatList, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 const SearchScreen = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredList, setFilteredList] = useState(null);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    const filteredList = list.filter((item) => item.name.toLowerCase().includes(query.toLowerCase()));
+    setFilteredList(filteredList);
+
+  };
+
     return (
         <View style={styles.container}>
-            <Text>Search Page</Text>
-            <StatusBar style="auto" />
+          <TextInput
+            value={searchQuery}
+            //onChangeText={(query) => handleSearch(query)}
+            placeholder="Enter search query"
+            style={styles.searchInput}
+          />
+          {/* {filteredList && (
+        <FlatList
+            data={filteredList}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+            <Text>{item.name}</Text>
+          )}
+        
+      )} */}
         </View>
     )
 }
@@ -16,7 +39,16 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      //justifyContent: 'center',
+    },
+    searchInput: {
+      borderColor: '#ccc',
+      borderRadius: 4,
+      borderWidth: 1,
+      fontSize: 16,
+      padding: 8,
+      width: '90%',
+      borderRadius: 20,
     },
   });
 
