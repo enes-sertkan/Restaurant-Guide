449282f870128736a6766e28ef9e0354831f1989
Humaiyun Uddin <uddin513@msn.com>
2022-12-09 20:13:16 -0500

    Create LocationScreen.js

---
+++ components/Home/LocationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const LocationScreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>Location Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default LocationScreen
